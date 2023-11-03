267068cd26603fd54e3ab635b8567a71f252dd2d
arnavjk007 <144872754+arnavjk007@users.noreply.github.com>
2023-11-03 12:49:23 -0700

    Add files via upload

---
+++ TicTacToe2/ContentView.swift
@@ -0,0 +1,85 @@
+//
+//  ContentView.swift
+//  TicTacToe2
+//
+//  Created by Arnav Kumar on 1/8/22.
+//
+
+import SwiftUI
+
+
+
+struct ContentView: View {
+    let columns: [GridItem] = [GridItem(.flexible()),
+         GridItem(.flexible()),
+         GridItem(.flexible()),]
+    
+    @State private var moves: [Move?] = Array(repeating: nil, count: 9)
+    @State private var isHumansTurn = true
+    
+    
+    var body: some View {
+        GeometryReader { geometry in
+            VStack(alignment: .center) {
+                //Spacer()
+            
+                LazyVGrid(columns: columns, spacing: 5) {
+                ForEach(0..<9) { i in
+                    ZStack(alignment: .center) {
+                        Rectangle()
+                            .foregroundColor(.red).opacity(0.75)
+                            .frame(width: geometry.size.width/3 - 15,
+                                   height: geometry.size.width/3 - 15)
+                            
+                        Image(systemName: moves[i]?.indicator ?? "")
+                            .resizable()
+                            .frame(width: 40, height: 40)
+                            .foregroundColor(.white)
+                        
+                        
+                    }
+                    .onTapGesture {
+                        if isSquareOccupied(in: moves, forIndex: i) {return}
+                        moves[i] = Move(player: isHumansTurn ? .human : .computer, boardIndex: i)
+                        isHumansTurn.toggle()
+                    }
+                    
+                }
+        
+            //Spacer()
+            
+        }
+        }
+            .padding(.top, 150)
+        }
+    }
+        
+        func isSquareOccupied(in moves: [Move?], forIndex index: Int) -> Bool {
+            return moves.contains(where: { $0?.boardIndex == index})
+        
+        
+}
+
+
+    enum Player {
+        case human, computer
+        
+    }
+    
+    struct Move {
+        let player: Player
+        let boardIndex: Int
+        
+        var indicator: String {
+            return player == .human ? "xmark" : "circle"
+        }
+    }
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+
+
+}
---
+++ TicTacToe2/TicTacToe2App.swift
@@ -0,0 +1,17 @@
+//
+//  TicTacToe2App.swift
+//  TicTacToe2
+//
+//  Created by Arnav Kumar on 1/8/22.
+//
+
+import SwiftUI
+
+@main
+struct TicTacToe2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
